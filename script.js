4bced5d163c24c9612bd64caf6961aa27c43d5cd
Majid <mjood0909@gmail.com>
2026-01-22 23:18:52 +0500

    Use BEM for class names

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
-const rootEl = document.querySelector("#root");
-const setGridBtn = document.querySelector(".set-grid-btn");
-const clearBtn = document.querySelector(".clear-btn");
+const canvasEl = document.querySelector("#canvas");
+const controls = document.querySelectorAll(".js-controls");
+const clearBtn = document.querySelector("#clear-btn");
 
-let squares = 16;
+const gridSliderValue = document.querySelector(".slider__value");
+const gridSlider = document.querySelector(".slider__input");
+
+let squareSize = 16;
 const gridSize = 500;
 
+let brushMode = "black";
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  console.log(`${r}, ${g}, ${b}`);
+  return `${r}, ${g}, ${b}`;
+}
+
 function renderGridSquares() {
-  rootEl.innerHTML = " ";
+  canvasEl.innerHTML = " ";
 
-  for (let i = 1; i <= squares * squares; i++) {
-    const squareEl = document.createElement("div");
+  for (let i = 1; i <= squareSize * squareSize; i++) {
+    const canvasCell = document.createElement("div");
 
-    squareEl.classList.add("square");
-    squareEl.style.width = gridSize / squares + "px";
-    squareEl.style.height = gridSize / squares + "px";
+    canvasCell.classList.add("workplace__cell");
+    canvasCell.style.width = gridSize / squareSize + "px";
+    canvasCell.style.height = gridSize / squareSize + "px";
 
-    rootEl.appendChild(squareEl);
+    canvasEl.appendChild(canvasCell);
   }
 
-  const allSquares = document.querySelectorAll(".square");
+  const allCells = document.querySelectorAll(".workplace__cell");
 
-  allSquares.forEach((square) => {
-    square.addEventListener("mouseenter", (e) => {
-      const element = e.target;
-      const color = getRandomColor();
-      if (!element.style.backgroundColor) {
-        element.style.backgroundColor = color;
+  allCells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
+      let rgb = brushMode === "rainbow" ? getRandomColor() : "0, 0, 0";
+
+      if (!e.target.style.backgroundColor && brushMode !== "eraser") {
+        e.target.style.backgroundColor = `rgb(${rgb})`;
+      } else if (brushMode === "eraser") {
+        e.target.style.backgroundColor = ``;
       }
     });
   });
 }
 
-function getRandomColor() {
-  let randomInt = () => Math.floor(Math.random() * 255 + 1);
-  console.log(`rgb(${randomInt()}, ${randomInt()}, ${randomInt()})`);
-  return `rgb(${randomInt()}, ${randomInt()}, ${randomInt()})`;
-}
+controls.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    controls.forEach((b) => {
+      if (b.classList.contains("primary-btn-front__active")) {
+        b.classList.remove("primary-btn-front__active");
+      }
+    });
 
-let i = 0;
-
-while (i < 1000) {
-  getRandomColor();
-  i++;
-}
-
-function getGrid() {
-  return prompt("Enter a number (1-100)");
-}
-
-setGridBtn.addEventListener("click", () => {
-  const newSquares = Number(getGrid());
-
-  console.log(newSquares);
-
-  if (!newSquares) {
-    alert("invalid value, please enter a number between 1 and 100");
-    return;
-  }
-
-  if (Number.isInteger(newSquares) && newSquares <= 100) {
-    squares = newSquares;
-    renderGridSquares();
-  }
+    brushMode = btn.id;
+    btn.classList.add("primary-btn-front__active");
+  });
 });
 
 clearBtn.addEventListener("click", () => {
   renderGridSquares();
+});
+
+gridSlider.addEventListener("change", (e) => {
+  const newSquares = Number(e.target.value);
+
+  if (Number.isInteger(newSquares) && newSquares <= 100) {
+    squareSize = newSquares;
+    gridSliderValue.textContent = `${newSquares}x${newSquares}`;
+    renderGridSquares();
+  }
 });
 
 renderGridSquares();
